3df1bee2b375827c67ee654647da416641a4b4a2
jeatzr <profesor.jeatienzar@iesazarquiel.es>
2023-04-19 19:10:59 +0200

    Beer and BigMac Price per country

--- precios-scrap.py
+++ precios-scrap.py
@@ -7,6 +7,9 @@ URL_BASE = 'https://preciosmundi.com/'
 continentes = ['europa', 'america', 'asia', 'africa', 'oceania']
 countries_list = []
 
+# buscamos en la página de cada continente los links
+# base de todos los países que guardaremos en countries_list
+
 for continente in continentes:
     web_indice = requests.get(URL_BASE + continente).content
     soup_indice = BeautifulSoup(web_indice, "html.parser")
@@ -20,11 +23,25 @@ for continente in continentes:
 
 print(countries_list)
 
+# recorremos la lista de URL de países
 for country in countries_list:
     web_restaurants = requests.get(
         country['url']+"precio-restaurantes").content
     soup_restaurants = BeautifulSoup(web_restaurants, "html.parser")
     rows = soup_restaurants.find("table").find_all("tr")
-    bigmac_price = rows[5].find_all("td")[2].text
-    print("Bigmac Price: "+bigmac_price)
+
+    # si solo hay tres columas el precio viene solo en Dólar y Euro
+    # por lo que el precio en dólar esta en la columna 1
+    if len(rows[0].find_all("td")) == 3:
+        i_dolar = 1
+    # en caso contraro suponemos que el precio en dólar está en columna 2
+    else:
+        i_dolar = 2
+
+    beer_price = rows[5].find_all("td")[i_dolar].find(string=True)
+    bigmac_price = rows[6].find_all("td")[i_dolar].find(string=True)
+
+    print(country['name'])
+    print("Bigmac menu price: " + bigmac_price)
+    print("Beer price: " + beer_price)
     time.sleep(5)
